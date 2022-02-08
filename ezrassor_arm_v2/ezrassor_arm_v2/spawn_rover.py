49f5bb6226581ed2649bb673081ba36b60f7c52c
Robert Forristall <robert.forristall@knights.ucf.edu>
2022-02-08 09:30:31 -0500

    Added paver arm model and rover model launch arg

--- ezrassor_arm_v2/ezrassor_arm_v2/spawn_rover.py
+++ ezrassor_arm_v2/ezrassor_arm_v2/spawn_rover.py
@@ -2,16 +2,28 @@ import os
 import rclpy
 import sys
 from gazebo_msgs.srv import SpawnEntity
+from ament_index_python.packages import get_package_share_directory
+
+import xacro
 
 def main(args=None):
     rclpy.init(args=args)
     node = rclpy.create_node('minimal_client')
     cli = node.create_client(SpawnEntity, '/spawn_entity')
 
-    content = sys.argv[1]
+    rover_model = sys.argv[1]
+
+    pkg_ezrassor_arm_v2 = get_package_share_directory('ezrassor_arm_v2')
+    if rover_model == 'arm':
+        model_uri = os.path.join(pkg_ezrassor_arm_v2, 'resource/ezrassor_arm_v2.xacro')
+    else:
+        model_uri = os.path.join(pkg_ezrassor_arm_v2, 'resource/ezrassor.xacro')
+
+    ezrassor_description_config = xacro.process_file(model_uri)
+    content = ezrassor_description_config.toxml()
 
     req = SpawnEntity.Request()
-    req_name = 'ezrassor'
+    req.name = 'ezrassor'
     req.xml = content
     req.robot_namespace = ""
     req.reference_frame = "world"
--- ezrassor_arm_v2/setup.py
+++ ezrassor_arm_v2/setup.py
@@ -5,8 +5,8 @@ data_files = []
 data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
 data_files.append(('share/' + package_name + '/launch', ['launch/arm_launch.py', 'launch/test_arm_launch.py']))
 data_files.append(('share/' + package_name + '/worlds', ['worlds/base.world']))
-data_files.append(('share/' + package_name + '/resource', ['resource/ezrassor.gazebo', 'resource/ezrassor.xacro', 'resource/macros.xacro', 'resource/materials.xacro', 'resource/model.sdf', 'resource/tutorial_model.sdf', 'resource/ezrassor_v2.xacro']))
-data_files.append(('share/' + package_name + '/meshes', ['meshes/base_station.dae', 'meshes/base_unit.dae', 'meshes/drum_arm.dae', 'meshes/drum.dae', 'meshes/wheel.dae']))
+data_files.append(('share/' + package_name + '/resource', ['resource/ezrassor.gazebo', 'resource/ezrassor.xacro', 'resource/macros.xacro', 'resource/materials.xacro', 'resource/model.sdf', 'resource/tutorial_model.sdf', 'resource/ezrassor_v2.xacro', 'resource/ezrassor_arm.gazebo', 'resource/ezrassor_arm_v2.xacro']))
+data_files.append(('share/' + package_name + '/meshes', ['meshes/base_station.dae', 'meshes/base_unit.dae', 'meshes/drum_arm.dae', 'meshes/drum.dae', 'meshes/wheel.dae', 'meshes/arm_camera.dae', 'meshes/grabber1.dae', 'meshes/grabber2.dae', 'meshes/link1.dae', 'meshes/link2.dae', 'meshes/link3.dae', 'meshes/link4.dae', 'meshes/link5.dae', 'meshes/link6.dae', 'meshes/link6.1.dae', 'meshes/platform.dae', 'meshes/rover_arm_back.dae', 'meshes/processor_housing.dae']))
 data_files.append(('share/' + package_name, ['package.xml']))
 
 setup(
